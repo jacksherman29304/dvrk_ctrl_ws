cf3c893eb8e3990d650de3cb91e046b7d82509cf
Jack <jacksherman29304@gmail.com>
2026-07-31 16:45:01 +0100

    added all peg_transfer operations to routines.py, main peg_transfer.py behaves like state machine

--- src/peg_task/peg_task/peg_transfer.py
+++ src/peg_task/peg_task/peg_transfer.py
@@ -19,7 +19,8 @@ from peg_control.tool_cmd import ToolCommand
 # Task imports
 from peg_task.config import load_config
 from peg_task.ros_interface import ObjectPoseClient, SimControl
-from peg_task.routines import PsmInit, get_object_pose, enter_scene
+from peg_task.routines import PsmInit, get_object_pose, init_handlers
+from peg_task.routines import enter_scene, grasp_block, lift_block, relocate_block, place_block
 from peg_task.motion import psm_to_pose
 
 
@@ -30,134 +31,35 @@ psm1 = ToolCommand('psm1')
 psm2 = ToolCommand('psm2')
 sim = SimControl()
 
+init_handlers(object_pose_client) # pass client-side handler to routines,py
+
 #------------------------- RUN TASK LOOP -------------------------#  
 def run_task(parameters):
-#------------------------- RESET + INIT -------------------------#  
-    params = parameters
+
+    params = parameters # parameters.yaml - offsets, objects etc.
     sim.reset_env() # Reset all simulation bodies (e.g. blocks, PSMs)
     time.sleep(2.0)
-    #T_base_w1, T_ee_base1, T_ee_w1 = PsmInit('psm1')
-    #T_base_w2, T_ee_base2, T_ee_w2 = PsmInit('psm2') # PSM initialisation
     T_base_w2, T_ee_base2, T_ee_w2 = PsmInit(psm=psm2, jp_init=params['init']['psm2_init'], jaw_init=params['init']['jaw_open'], max_wait=10)
     time.sleep(3.0)
 
+    target_arm = psm2
     #------------------------- GET BLOCK  -------------------------#   
-    # 
-    T_block_w = get_object_pose(object_pose_client, params['target_block'])
+    T_block_w = get_object_pose(params['target_block'])
 
     # 'Claw-grabber' gripper orientation from init function - was a commanded jaw position, now translated into RPY
     R_desired_w = T_ee_w2.M # Target grasp orientation in world frame
     R_offset = (T_block_w.M).Inverse() * R_desired_w # Desired rotation offset converted to block frame
 
 #------------------------- ENTRANCE OPERATION -------------------------#   
-    # # ENTRANCE
-    position_offset = Vector(-0.004, 0.01, 0.06) # World frame position offset (6cm above the block)
-    # p_entrance_w = T_block_w.p + position_offset # Add position offset to block in world
-
-    # T_block_entrance = Frame(T_block_w.M * R_offset, p_entrance_w) # Apply R_offset relative to block's CURRENT rotation (composes, tracks the block) - this is still in the WORLD FRAME
-    # T_cmd_entrance = T_base_w2.Inverse() * T_block_entrance # Convert block + offset in WORLD to block + offset in BASE for servo_cmd
-
-    T_cmd_entrance = enter_scene(T_block_w, position_offset, R_offset, T_base_w2)
-    
-    Enter = False # Interpolation success flag
-    print(f"Entering scene for {params['target_block']}")
-    psm_to_pose(psm=psm2, target_pose=T_cmd_entrance, success_flag=Enter, max_delta=0.01, pos_deadband=0.005)
-    time.sleep(2.0)
-
-#------------------------- GRAB OPERATION -------------------------#
-    # GRASP
-    position_offset = Vector(-0.002,0.001, 0.015)
-    p_grasp_w = T_block_w.p + position_offset
-
-    T_block_grasp = Frame(T_block_w.M * R_offset, p_grasp_w)
-    T_cmd_grasp = T_base_w2.Inverse() * T_block_grasp
-
-    Grasp = False # Interpolation success flag
-    print(f"Moving to {params['target_block']}")
-    psm_to_pose(psm=psm2, target_pose=T_cmd_grasp, success_flag=Grasp, max_delta=0.003, pos_deadband=0.005)
-    print(f"Arrived at {params['target_block']}")
-
-    # Close Jaws
-    time.sleep(2.0)
-    for _ in range(40): # give time for grip to register
-        psm2.set_jaw(0.02)
-        time.sleep(0.05)
-
+    enter_scene(target_arm)
+#------------------------- GRASP OPERATION -------------------------#
+    grasp_block(target_arm)
 # #------------------------- LIFT OPERATION -------------------------#  
-    # LIFT
-
-    # Needed to update T_ee_w2 and block position in wold so just hard-coded it here 
-    # In future, edit psm_init function to have a boolean that either recalcs these variables or recalcs and commands to init pos
-    # Need to carry over the object format function here as well
-    T_base_w = ps_to_frame(psm2.T_b_w) # Grab base in world pose from psm2/T_b_w and convert to frame
-    T_ee_base = ps_to_frame(psm2.measured_cp) # Grab ee relative to base pose from psm2/measured_cp and convert to frame
-    T_ee_w2 = T_base_w * T_ee_base # ee in world is multiplication of previous transforms
-
-    target_block = params['target_block']
-    block1 = object_pose_client.get_object_pose(target_block) # block1 in world frame
-    T_block_w = rbs_to_frame(block1) # Convert block1 RigidBodyState to Frame
-    T_block_ee = T_ee_w2.Inverse() * T_block_w # Grasp relationship between block and end-effector (block in end effector)
-
-    #position_offset = Vector(-0.004, 0.01, 0.06) # World frame position offset (6cm above the block)
-    position_offset = Vector(0,0,0.05)
-    p_lift_w = T_block_w.p + position_offset # blocks position in world + z-offset (lift block 6cm above blocks current position)
-    T_block_lift = Frame(T_block_w.M, p_lift_w) # lift block frame - target rotation and position, why am I using the blocks rotation?
-
-    ####T_cmd_base = T_base_w.Inverse() * T_block_lift*T_block_ee.Inverse()
-
-    T_desired_ee_w = T_block_lift * T_block_ee.Inverse() #T_block_ee.Inverse()* T_ee_w2.Inverse() * T_block_lift # target position in frame of ee 
-
-    #T_desired_ee_w = T_block_lift * T_block_ee.Inverse() # Invert the grasp offset to solve for the ee pose that puts the block there in the world-frame
-    T_cmd_lift = T_base_w.Inverse()*T_desired_ee_w# Convert from ee in world-frame to base-frame for servo_cp
-    # T_desired_ee_w = T_block_lift.Inverse()*T_ee_w2
-    # T_cmd_lift = T_desired_ee_w.Inverse()*T_ee_base.Inverse
-
-    Lift = False
-    #psm_to_pose(psm=psm2, target_pose=T_cmd_lift, success_flag=Lift, max_delta=0.003, pos_deadband=0.005, rot_deadband=0.01, log_path=f"/home/dvrk-team/internship/logs/lift_{time.strftime('%Y%m%d_%H%M%S')}.csv")
-    psm_to_pose(psm=psm2, target_pose=T_cmd_lift, success_flag=Lift, max_delta=0.003, pos_deadband=0.005, rot_deadband=0.01, control_speed=0.05, timeout=30)
-    print(f"Lifted{target_block}")
-
-    time.sleep(2.0)
-
-
+    lift_block(target_arm)
 # #------------------------- RELOCATE OPERATION -------------------------#  
-    peg10 = object_pose_client.get_object_pose(params['target_peg'])
-    T_peg_w = rbs_to_frame(peg10)
-
-    #local_offset = Frame(Rotation.RPY(0,0,0), Vector(0,0,0.05)) # move block above the peg
-    #T_relocate = T_peg_w * local_offset # target in world - either way works - I think I prefere this
-    position_offset = Vector(0,0,0.05)
-    p_relocate_w = T_peg_w.p + position_offset
-    T_relocate = Frame(T_peg_w.M, p_relocate_w) # Target position in world
-
-    T_desired_ee_w = T_relocate * T_block_ee.Inverse()
-    T_cmd_relocate = T_base_w.Inverse() * T_desired_ee_w
-
-
-    Relocate = False
-    print("Relocating")
-    #psm_to_pose(psm=psm2, target_pose=T_cmd_lift, success_flag=Lift, max_delta=0.003, pos_deadband=0.005, rot_deadband=0.01, log_path=f"/home/dvrk-team/internship/logs/lift_{time.strftime('%Y%m%d_%H%M%S')}.csv")
-    psm_to_pose(psm=psm2, target_pose=T_cmd_relocate, success_flag=Relocate, max_delta=0.03, pos_deadband=0.0005, rot_deadband=0.01, control_speed=0.01, timeout=30)
-    print(f"Relocated {target_block} to {params['target_peg']}")
-
-    time.sleep(2.0)
-
+    relocate_block(target_arm)
 # #------------------------- PLACE OPERATION -------------------------#  
-
-    local_offset = Frame(Rotation.RPY(0,0,-np.pi/9), Vector(0,0,0.02)) # relocate block to bottom og peg - with 45 degree yaw to prevent arm getting in the way
-    T_place = T_peg_w * local_offset
-
-    T_desired_ee_w = T_place * T_block_ee.Inverse()
-    T_cmd_place = T_base_w.Inverse() * T_desired_ee_w
-
-    Place = False
-    print("Placing")
-    psm_to_pose(psm=psm2, target_pose=T_cmd_place, success_flag=Place, max_delta=0.03, pos_deadband=0.005, rot_deadband=0.01, control_speed=0.01, timeout=30)
-    print(f"Placed {target_block} on {params['target_peg']}")
-    psm2.set_jaw(0.08) # open jaw
-    time.sleep(2.0)
-    psm_to_pose(psm=psm2, target_pose=T_cmd_relocate, success_flag=Place, max_delta=0.03, pos_deadband=0.0005, rot_deadband=0.01, control_speed=0.01, timeout=30)
-
+    place_block(target_arm)
 
 # Moved thread spinners to main function
 # This is ros2 run command is looking at setup.py wrapper, which references 'main'
--- src/peg_task/peg_task/routines.py
+++ src/peg_task/peg_task/routines.py
@@ -2,12 +2,30 @@
  
 import time
 import numpy as np
-from PyKDL import Vector, Frame
+from PyKDL import Vector, Frame, Rotation
 
 from peg_math.conversions import ps_to_frame, rbs_to_frame
+from peg_task.config import load_config
+from peg_task.motion import psm_to_pose
+
+#------------------------- GLOBALS -------------------------#
+_object_pose_client = None
+_R_offset = None
+#------------------------- NODE HANDLERS -------------------------#
+
+
+# Get client-side handler for 'GetObjectPose' - eventually to be replaced with perception layer
+def init_handlers(object_pose_client):
+    global _object_pose_client
+    _object_pose_client = object_pose_client
+
+
+parameters = load_config()
+global _get_parameter
+_get_parameter = parameters 
 
 #------------------------- PSM INIT -------------------------#
-# # Check CRTK BASED CONTROL SCRIPT AND ALSO ORIGINAL PEG TRANSFER
+# Check CRTK BASED CONTROL SCRIPT AND ALSO ORIGINAL PEG TRANSFER
 def PsmInit(psm, jp_init, jaw_init, max_wait):
 
     psm.move_jp(jp_init)
@@ -35,21 +53,163 @@ def PsmInit(psm, jp_init, jaw_init, max_wait):
 
     return T_base_w, T_ee_base, T_ee_w
 
-def get_object_pose(pose_client, target_obj):
-    object_pose = rbs_to_frame(pose_client.get_object_pose(target_obj)) # target object in world (Frame)
+
+#-------------------------  GET OBJECT POSE -------------------------#
+# eventually remove pose_client
+def get_object_pose(target_obj):
+    object_pose = rbs_to_frame(_object_pose_client.get_object_pose(target_obj)) # target object in world (Frame)
 
     return object_pose
 
-def enter_scene(target_obj, entrance_pos, entrance_rotation, base_in_world):
-    entry_pos_v = Vector(entrance_pos)
-    entry_position = target_obj.p + entry_pos_v # Add position offset to target block in world
-    entry_frame = Frame(target_obj.M * entrance_rotation, entry_position)  # Apply R_offset relative to block's CURRENT rotation (composes, tracks the block) - this is still in the WORLD FRAME
-    entry_command = base_in_world.Inverse() * entry_frame # Convert block + offset in WORLD to block + offset in BASE for servo_cmd
 
-    return entry_command
-     
+#------------------------- SUB-ROUTINES -------------------------#
+
+# commanding gripper to certain pose
+def move_gripper_to_pose(psm, target_object, rot_offset, pos_offset, max_delta, pos_deadband, rot_deadband):
+
+    base_w = ps_to_frame(psm.T_b_w) # base in world pose, convert to Frame
+
+    object_w = get_object_pose(target_object) # get target object pose in world
+
+    local_offset = Frame(rot_offset, pos_offset) # compile local offset into a Frame
+
+    target_pose_w = object_w * local_offset # apply local offset to target object in world = target final pose
+
+    target_pose_b = base_w.Inverse() * target_pose_w # convert target object with offset in world into the base frame for PSM commands
+
+    success_flag = False
+    psm_to_pose(psm=psm, target_pose=target_pose_b, success_flag=success_flag, 
+                max_delta=max_delta, pos_deadband=pos_deadband, rot_deadband=rot_deadband)
+
+# commanding gripper with grasped item to certain pose
+def move_grasped_object_to_pose(psm, grasped_object, target_object, rot_offset, pos_offset, max_delta, pos_deadband, rot_deadband):
+
+    base_w = ps_to_frame(psm.T_b_w) # base in world pose, convert to Frame
+
+    ee_base = ps_to_frame(psm.measured_cp) # end-effector in base pose, converted to Frame
+
+    ee_w = base_w * ee_base # end-effector in world
+
+    object_w = get_object_pose(grasped_object) # get grasped object pose in world
+
+    object_ee = ee_w.Inverse() * object_w # block in end-effector (grasp relationship)
+
+    local_offset = Frame(rot_offset, pos_offset) # compile local offset into a Frame
+
+    target_pose_w = get_object_pose(target_object) * local_offset # apply local offset to target object in world = target final pose
+
+    target_pose_grasp_w = target_pose_w * object_ee.Inverse() # target final pose with grasp offset applied
+
+    target_pose_b = base_w.Inverse() * target_pose_grasp_w # convert target object with offset in world into the base frame for PSM commands
+
+    success_flag = False
+    psm_to_pose(psm=psm, target_pose=target_pose_b, success_flag=success_flag, 
+                max_delta=max_delta, pos_deadband=pos_deadband, rot_deadband=rot_deadband)
+
+
+#------------------------- TASK ROUTINES -------------------------#
+
+
+def enter_scene(psm):
+    T_base_w = ps_to_frame(psm.T_b_w) # Grab base in world pose from psm2/T_b_w and convert to frame
+    T_ee_base = ps_to_frame(psm.measured_cp) # Grab ee relative to base pose from psm2/measured_cp and convert to frame
+    T_ee_w = T_base_w * T_ee_base # ee in world is multiplication of previous transforms
+
+    T_block_w = get_object_pose(_get_parameter['target_block'])
+
+    R_desired_w = T_ee_w.M # Target grasp orientation in world frame
+
+    global _R_offset
+
+    _R_offset = (T_block_w.M).Inverse() * R_desired_w # Desired rotation offset converted to block frame
+
+    print(f"Entering scene for {_get_parameter['target_block']}")
+
+    move_gripper_to_pose(psm=psm, 
+                    target_object=_get_parameter['target_block'], 
+                    rot_offset= _R_offset, 
+                    pos_offset=Vector(*_get_parameter['entrance']['pos_offset']), 
+                    max_delta=_get_parameter['entrance']['max_delta'], 
+                    pos_deadband=_get_parameter['entrance']['pos_deadband'], 
+                    rot_deadband=_get_parameter['entrance']['rot_deadband'])
+
+    print("Entered Scene")
+
+    time.sleep(2.0)
+
+def grasp_block(psm):
+
+    print(f"Moving to grasp {_get_parameter['target_block']}")
+
+    move_gripper_to_pose(psm=psm,
+                    target_object=_get_parameter['target_block'],
+                    rot_offset = _R_offset,
+                    pos_offset= Vector(*_get_parameter['grasp']['pos_offset']),
+                    max_delta=_get_parameter['grasp']['max_delta'], 
+                    pos_deadband=_get_parameter['grasp']['pos_deadband'], 
+                    rot_deadband=_get_parameter['grasp']['rot_deadband'])
+
+    print(f'Arrived at {_get_parameter['target_block']}')
+
+    time.sleep(2.0)
+
+    for _ in range(40):
+        psm.set_jaw(0.02)
+        time.sleep(0.05)
+
+    print(f'Grasped {_get_parameter['target_block']}')
+
+def lift_block(psm):
+
+    print(f"Lifting {_get_parameter['target_block']}")
+
+    move_grasped_object_to_pose(psm=psm,
+                                grasped_object=_get_parameter['target_block'],
+                                target_object=_get_parameter['target_block'],
+                                rot_offset = Rotation.RPY(*_get_parameter['lift']['rot_offset']),
+                                pos_offset = Vector(*_get_parameter['lift']['pos_offset']),
+                                max_delta=_get_parameter['lift']['max_delta'], 
+                                pos_deadband=_get_parameter['lift']['pos_deadband'], 
+                                rot_deadband=_get_parameter['lift']['rot_deadband'])
+
+    print(f"Lifted {_get_parameter['target_block']}")
+
+    time.sleep(2.0)
+    
+def relocate_block(psm):
+
+    print(f"Relocating {_get_parameter['target_block']} to {_get_parameter['target_peg']}")
+
+
+    move_grasped_object_to_pose(psm=psm,
+                                grasped_object=_get_parameter['target_block'],
+                                target_object=_get_parameter['target_peg'],
+                                rot_offset = Rotation.RPY(*_get_parameter['relocate']['rot_offset']),
+                                pos_offset = Vector(*_get_parameter['relocate']['pos_offset']),
+                                max_delta=_get_parameter['relocate']['max_delta'], 
+                                pos_deadband=_get_parameter['relocate']['pos_deadband'], 
+                                rot_deadband=_get_parameter['relocate']['rot_deadband'])
+
+    print(f"Relocated {_get_parameter['target_block']} to {_get_parameter['target_peg']}")
+
+    time.sleep(2.0)
 
     
+def place_block(psm):
 
+    print(f'Placing {_get_parameter['target_block']} on {_get_parameter['target_peg']}')
 
-    
+    move_grasped_object_to_pose(psm=psm,
+                                grasped_object=_get_parameter['target_block'],
+                                target_object=_get_parameter['target_peg'],
+                                rot_offset = Rotation.RPY(*_get_parameter['place']['rot_offset']),
+                                pos_offset = Vector(*_get_parameter['place']['pos_offset']),
+                                max_delta=_get_parameter['place']['max_delta'], 
+                                pos_deadband=_get_parameter['place']['pos_deadband'], 
+                                rot_deadband=_get_parameter['place']['rot_deadband'])
+
+    psm.set_jaw(0.08)
+
+    print(f"Placed {_get_parameter['target_block']} on {_get_parameter['target_peg']}")
+
+    time.sleep(2.0)
